dbf694c9da3e599bbc5803d51dc5508d9eb715b5
chanjunren <chanjunren@gmail.com>
2020-05-14 20:18:49 +0800

    Tutorial 25
    React Fragments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import LifecycleA from './components/LifecycleA';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
-      <LifecycleA></LifecycleA>
+      <Table/>
     </div>
   );
 }
---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FragmentDemo() {
+    return (
+        <React.Fragment>
+            <div>
+                FragmentDemo
+            </div>
+            <p>This describes the Fragment Demo Component</p>
+        </React.Fragment>
+    )
+}
+
+export default FragmentDemo
